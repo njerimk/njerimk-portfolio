966f9894d1a00b96f9feccebfdfbde993c9d524d
njerimk <nmurugikamau@gmail.com>
2023-11-02 15:56:19 -0400

    completed basic tailwindcss formatting of main page, components about and work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import BioPhoto from '../BioPhoto.JPG'
+import NjeriBioPic from '../NjeriBioPic.png'
 
 export default function About(props) {
     
 
     return (
-        <div className="w-4/5 rounded border border-2 border-pink-300 m-10 grid grid-rows-1 grid-cols-2" id="about-page">
-            <div className="h-0.5 col-start-2 col-span-1 row-start-1 row-span-1">
+        <div className="w-5/6 font-mono m-auto rounded border border-2 border-pink-300 mt-10 grid grid-rows-1 grid-cols-2 gap-4 grid-column:auto" id="about-page">
+            <div className="h-auto m-10 col-start-1 col-span-1 row-start-1 row-span-1">
                 <h1 className="font-mono inline text-3xl text-pink-400">02.</h1>
-                <h1 className="font-san inline text-4xl">About Me</h1>
+                <h1 className="font-mono inline text-3xl">About Me</h1>
             </div> 
-            <div className="w-1/2 mt-20 rounded border-2 border-pink-300 p-5 mb-10 col-start-2 col-span-1 row-start-1 row-span-1">
+            <div className="h-auto m-auto col-start-2 col-span-1 row-start-1 row-span-1 z-index: 3">
                 
-                <img className="max-w-sm filter grayscale hover:grayscale-0 rounded-lg" src={BioPhoto}></img>   
+                <img className="h-96 object-contain z-index:2" src={NjeriBioPic}></img>   
             </div>
-            <div>              
-                <p className="text-2xl m-40 font-san col-start-1 col-span-1 row-start-2 row-span-1 mb-20">
+            <div className="mt-20 h-0.5 col-start-1 col-span-1 row-start-1 row-span-1">              
+                <p className="text-2xl p-10 m-auto font-san">
                     Hello! My name is Njeri (sounds like 'Jeri') and I am a software engineer residing in Maryland. 
                     I enjoy creating things for the web (applications and illustrations)
                 </p>
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import CartonCo from "../Screen Shot 2021-03-05 at 11.07.57 AM.png"
-import SailorCards from "../SailorSenshiCover.png"
+import CartonCo from "../CartonCo.gif"
+import SailorCards from "../sailorsenshi.gif"
 // import Github from "./Portfolio Site/25231.png"
 // import Youtube from "./Portfolio Site/yt_icon.png"
 export default function Work() {
     return (
         <>
-        <div className="mt-10 grid grid-rows-5 grid-col-5" id="work-page">
-                <div className="grid grid-flow-col block h-1 p-10 mb-10">
+        <div className="mx-20 mt-10 grid grid-rows-2 grid-col-5" id="work-page">
+                <div className="grid grid-flow-col block h-1 p-10">
                     <div className="row-start-1 row-span-1 col-start-1 col-span-2">
                         <h1 className="font-mono inline mr-2 text-3xl text-pink-400 ">03.</h1>
                         <h1 className="font-san inline text-4xl">Work </h1> 
@@ -44,16 +44,16 @@ export default function Work() {
                     
                     
                 </div>
-                <div className="grid grid-flow-col block grid-col-2 grid-rows-2 p-10">
-                     <div className="text-left row-start-3 row-span-1 col-start-2 col-span-2">
+                <div className="w-auto grid grid-flow-col block grid-col-2 grid-rows-2 pb-5">
+                     <div className="text-left row-start-2 row-span-1 col-start-2 col-span-2">
                         <h3 className="font-mono text-pink-400"> Featured Project</h3>
                         <h3 className="font-san inline text-2xl">Sailor Senshi Cards</h3>
                     </div>
-                    <div className="row-start-3 row-span-1 col-start-3 col-span-2 rounded border p-2">
-                         <img className="m-5" src={SailorCards} alt="sailorcards"></img>   
+                    <div className="mr-10 row-start-3 row-span-1 col-start-3 col-span-2 rounded border">
+                         <img className="m-auto" src={SailorCards} alt="sailorcards"></img>   
                     </div>
                     <div className="text-left mt-20 col-start-2 col-span-2 row-start-3 row-span-1">
-                        <p className="mr-48 font-san text-xl bg-pink-300 w-98 text-white rounded border p-5">
+                        <p className="w-98 mr-48 font-san text-xl bg-pink-300 w-98 text-white rounded border p-5">
                             A matching card game with a Sailor Moon Theme
                         </p>
                         <div className="row-start-4 row-span-1 col-start-1 col-span-1">
